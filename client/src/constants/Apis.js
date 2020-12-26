c2746d72bfdb969be2f3d3fdbf9409e1ea0a3c8b
Jawad Ali <ahads62426@gmail.com>
2020-12-26 17:55:55 +0500

    deployed

--- client/src/constants/Apis.js
+++ client/src/constants/Apis.js
@@ -1,5 +1,5 @@
 export const LOCAL_URL = 'http://localhost:8000/'
-export const PUBLIC_URL = 'https://bizinissify.herokuapp.com/'
+export const PUBLIC_URL = 'https://dinemateapp.herokuapp.com/'
 export const BASE_URL = LOCAL_URL
 
 export const API_ENDPOINTS = {
