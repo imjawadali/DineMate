bfcef881f912fc58f428189cfe4f646a5233c6d3
Jawad Ali <ahads62426@gmail.com>
2021-01-04 02:24:02 +0500

    generalizedEpic added

--- client/src/redux/epics/admin/loginEpic.js
+++ client/src/redux/epics/admin/loginEpic.js
@@ -2,6 +2,7 @@ import { switchMap } from 'rxjs/operators'
 import { ofType } from 'redux-observable'
 
 import { customisedAction } from '../../actions'
+import { generalizedEpic } from '../generalizedEpic'
 import {
   ADMIN_SIGN_IN,
   ADMIN_SIGN_IN_FAILURE,
@@ -17,32 +18,17 @@ export class loginEpic {
       ofType(ADMIN_SIGN_IN),
       switchMap(
         async ({ payload: { email, password } }) => {
-          try {
-            const response = await RestClient.post(API_ENDPOINTS.admin.login, {
-              email,
-              password,
-            });
-            const { status, data: resObj, problem } = response;
-            if (status && status === 200) {
+          return generalizedEpic(
+            'post', 
+            API_ENDPOINTS.admin.login,
+            { email, password },
+            (resObj) => {
               setItem('admin', resObj);
               RestClient.setHeader('Authorization', resObj.id);
               return customisedAction(SET_SESSION, { admin: resObj });
-            }
-            console.log(response)
-            if (status && (status === 401 || status === 422 || status === 503)) {
-              return customisedAction(ADMIN_SIGN_IN_FAILURE, { message: resObj.msg, type: 'error' })
-            }
-            if (problem && problem === 'NETWORK_ERROR') {
-              return customisedAction(ADMIN_SIGN_IN_FAILURE, { message: 'Network Error while Signing-In Admin!', type: 'error' })
-            }
-            if (problem && problem === 'TIMEOUT_ERROR') {
-              return customisedAction(ADMIN_SIGN_IN_FAILURE, { message: 'Timeout Error while Signing-In Admin!', type: 'error' })
-            }
-            return customisedAction(ADMIN_SIGN_IN_FAILURE, { message: 'Unknown Error while Signing-In Admin!', type: 'error' })
-          } catch (error) {
-            console.log('ADMIN_SIGN_IN Unknown Error', error)
-            return customisedAction(ADMIN_SIGN_IN_FAILURE, { message: error.message, type: 'error' })
-          }
+            },
+            ADMIN_SIGN_IN_FAILURE
+          )
         }
       )
     );
---
+++ client/src/redux/epics/generalizedEpic.js
@@ -0,0 +1,28 @@
+import { RestClient } from '../../services/network'
+import { customisedAction } from '../actions'
+
+export const generalizedEpic = async (method, url, data, successCallback, failureAction) => {
+    try {
+        let response
+        if (method === 'get') {
+            response = await RestClient.get(url)
+        } else response = await RestClient.post(url, data)
+        const { status, data: resObj, problem } = response;
+        if (status && status === 200) {
+          return successCallback(resObj)
+        }
+        if (status && (status === 401 || status === 422 || status === 503)) {
+          return customisedAction(failureAction, { message: resObj.msg, type: 'error' })
+        }
+        if (problem && problem === 'NETWORK_ERROR') {
+          return customisedAction(failureAction, { message: `Network Error while ${failureAction.replace('_FAILURE', '')}!`, type: 'error' })
+        }
+        if (problem && problem === 'TIMEOUT_ERROR') {
+          return customisedAction(failureAction, { message: `Timeout Error while ${failureAction.replace('_FAILURE', '')}!`, type: 'error' })
+        }
+        return customisedAction(failureAction, { message: `Unknown Error while ${failureAction.replace('_FAILURE', '')}!`, type: 'error' })
+    } catch (error) {
+        console.log('ADMIN_SIGN_IN Unknown Error', error)
+        return customisedAction(failureAction, { message: error.message, type: 'error' })
+    }
+};
