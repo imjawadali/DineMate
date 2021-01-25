0031a75c86c9acd0e1b69caf726ac523aee5226c
Jawad Ali <ahads62426@gmail.com>
2021-01-25 19:11:15 +0500

    Dashboard UI done

--- client/src/views/Admin/Dashboard/RestaurantAdmin/index.js
+++ client/src/views/Admin/Dashboard/RestaurantAdmin/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
 
+import { DashboardBatches } from '../../../../components'
+
 function RestaurantAdmin(props) {
   return (
     <div className="Container">
-      <h2>Restaurant Admin Dashboard</h2>
+      <h2>Restaurant Dashboard</h2>
+      <div className="DashBoardContainer">
+        <div className="DashBoardSectionsContainer">
+          <DashboardBatches
+            text="10 Open Checks"
+            iconName="fa fa-coffee"
+            onClick={() => null}
+          />
+          <DashboardBatches
+            text="100 Closed Checks"
+            iconName="fa fa-ban"
+            onClick={() => null}
+          />
+        </div>
+        <div className="DashBoardSectionsContainer">
+          <DashboardBatches
+            text="10 Active Tables"
+            iconName="fa fa-table"
+            onClick={() => null}
+          />
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/views/Admin/Dashboard/SuperAdmin/index.js
+++ client/src/views/Admin/Dashboard/SuperAdmin/index.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
+import { DashboardBatches } from '../../../../components'
+
+import '../styles.css'
+
 function SuperAdmin(props) {
   return (
     <div className="Container">
-      <h2>Super Admin Dashboard</h2>
+      <h2>Dashboard</h2>
+      <div className="DashBoardContainer">
+        <div className="DashBoardSectionsContainer">
+          <DashboardBatches
+            text="200 Restaurants"
+            iconName="fa fa-cutlery"
+            onClick={() => null}
+          />
+          <DashboardBatches
+            text="4000 Users"
+            iconName="fa fa-users"
+            onClick={() => null}
+          />
+        </div>
+        <div className="DashBoardSectionsContainer">
+          <DashboardBatches
+            text="500 QR Codes"
+            iconName="fa fa-qrcode"
+            onClick={() => null}
+          />
+        </div>
+      </div>
     </div>
   )
 }
