94fb4e0828ead7d999249cdf3f055894364c3b2e
Jawad Ali <ahads62426@gmail.com>
2021-02-07 01:13:15 +0500

    . . .

--- client/src/constants/Apis.js
+++ client/src/constants/Apis.js
@@ -1,7 +1,7 @@
 export const LOCAL_URL = 'http://localhost:8000/'
 export const PUBLIC_URL = 'http://ec2-52-15-148-90.us-east-2.compute.amazonaws.com/dev/'
 export const CUSTOMER_APP_URL = 'http://ec2-52-15-148-90.us-east-2.compute.amazonaws.com/'
-export const BASE_URL = LOCAL_URL
+export const BASE_URL = '/dev/'
 
 export const API_ENDPOINTS = {
     admin: {
@@ -12,7 +12,7 @@ export const API_ENDPOINTS = {
         generateQrs: 'admin/generateQrs',
         getAllRestaurants: 'admin/getAllRestaurants',
         getExistingQrs: 'admin/getExistingQrs',
-        setTableName: '/admin/setTableName',
+        setTableName: 'admin/setTableName',
         getSuperAdminDashboard: 'admin/getSuperAdminDashboard',
         addCategory: 'admin/addCategory',
         getCategories: 'admin/getCategories',
