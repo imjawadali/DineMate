d823cdea7653083b356d8b6069232e6c28d6b111
Jawad Ali <ahads62426@gmail.com>
2021-05-02 11:10:03 +0500

    ...

--- index.js
+++ index.js
@@ -14,15 +14,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 require('./apis/AdminAPIS')(app)
 require('./apis/CustomerAPIS')(app)
 
-app.use(express.static('home/build'))
 app.use(express.static('client/build'))
 app.use(express.static('customer/build'))
 
 app.get('/client', (req, res) => res.redirect('/client/admin'))
 app.get('/client/*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-app.get('/customer', (req, res) => res.redirect('/customer/restaurants'))
-app.get('/customer/*', (req, res) => res.sendFile(path.resolve(__dirname, 'customer', 'build', 'index.html')))
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'home', 'build', 'index.html')))
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'customer', 'build', 'index.html')))
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT)
