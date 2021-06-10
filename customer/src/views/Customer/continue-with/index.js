9735073c3f149389ab76bb9b41dce279ee190b5d
Shiraz Ahmed <sherrazahmed.97@gmail.com>
2021-06-10 20:36:34 +0500

    Continue with page

---
+++ customer/src/views/Customer/continue-with/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import image from '../../../assets/ios-android.jpg';
+import google from '../../../assets/google.png';
+import apple from '../../../assets/apple.png';
+import computer from '../../../assets/computer.png';
+import './styles.css';
+
+function ContinueWith() {
+
+    return (
+        <div className="continue-with">
+            <div className="flex-1">
+                <img className="ios-android" src={image} />
+            </div>
+
+            <div className="flex-1" style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+                <div style={{ margin: 50, fontSize: 24, fontWeight: 'bolder' }}>Get the App & Place Order</div>
+
+                <div style={{ display: 'flex' }}>
+                    <div style={{ backgroundColor: 'black', borderRadius: 21, paddingTop: 4, margin: '0 5px' }}>
+                        <img className="play-store" src={google} />
+                    </div>
+
+                    <div style={{ backgroundColor: 'black', borderRadius: 21, paddingTop: 4, margin: '0 5px' }}>
+                        <img className="play-store" src={apple} />
+                    </div>
+                </div>
+
+                <div style={{ margin: 50, fontSize: 24, fontWeight: 'bolder' }}>Or</div>
+
+                <div style={{ marginBottom: 50, fontSize: 24, fontWeight: 'bolder' }}>Start Using</div>
+
+                <div style={{ display: 'flex', marginBottom: 20, cursor: 'pointer', background: '#0000001c', padding: 10, borderRadius: 5, alignItems: 'center' }}>
+                    <img className="computer-img" src={computer} />
+                    <span style={{ margin: 10, fontSize: 24, fontWeight: 'bolder' }}>The Web</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ContinueWith
