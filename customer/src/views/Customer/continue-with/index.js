8f71f5a07aba8ea880494265f15d3ca2df86d5d1
Jawad Ali <ahads62426@gmail.com>
2021-06-22 01:52:05 +0500

    intermediary screen minor fixes

--- customer/src/views/Customer/continue-with/index.js
+++ customer/src/views/Customer/continue-with/index.js
@@ -9,14 +9,14 @@ function ContinueWith() {
 
     return (
         <div className="continue-with">
-            <div className="flex-1">
+            <div className="flex-1 flex justify-center">
                 <img className="ios-android" src={image} />
             </div>
 
             <div className="flex-1" style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
                 <div style={{ margin: 50, fontSize: 24, fontWeight: 'bolder' }}>Get the App & Place Order</div>
 
-                <div style={{ display: 'flex' }}>
+                <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
                     <div style={{ backgroundColor: 'black', borderRadius: 21, paddingTop: 4, margin: '0 5px' }}>
                         <img className="play-store" src={google} />
                     </div>
