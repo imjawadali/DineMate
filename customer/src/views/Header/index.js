b6ddbc903ff63bce8bb6deae769df25f4620b7e0
muhammadmuhammad <muhammadmuhammadshoaib241@gmail.com>
2021-07-03 19:46:00 +0500

    sidebar & special instruction issue

--- customer/src/views/Header/index.js
+++ customer/src/views/Header/index.js
@@ -29,6 +29,8 @@ const Header = props => {
     const [addedAddons, setAddedAddons] = useState({});
     const [editedQuantity, setEditedQuantity] = useState('')
 
+    const [specialInstructions, setSpecialIntstruction] = useState("")
+
     let dispatch = useDispatch()
 
     let submitTakeOrder = useSelector(({ orderReducer }) => orderReducer.cartTakeItem)
@@ -106,7 +108,7 @@ const Header = props => {
                     "name": a.name,
                     "price": a.price,
                     "totalPrice": a.totalPrice,
-                    "specialInstructions": a.shortDescription,
+                    "specialInstructions": a.specialInstructions,
                     "addOns": a.addOns,
                     "restaurantId": a.restaurantId,
                     "status": true,
@@ -124,7 +126,7 @@ const Header = props => {
                         "name": a.name,
                         "price": a.price,
                         "totalPrice": a.totalPrice,
-                        "specialInstructions": a.shortDescription,
+                        "specialInstructions": a.specialInstructions,
                         "addOns": a.addOns,
                         "restaurantId": a.restaurantId,
                         "status": true,
@@ -371,8 +373,8 @@ const Header = props => {
 
     const [editInded, setEditInded] = useState('')
 
-    function editItem(id, restId, addons, i, quantity) {
-        console.log(menu, id)
+    function editItem(id, restId, addons, i, quantity, item) {
+        // console.log(menu, id)
         setSelectedItem(
             menu.filter((a, i) => a.id === id)[0]
         )
@@ -381,6 +383,8 @@ const Header = props => {
         setAddedAddons(addons)
         setEditInded(i)
         setEditedQuantity(quantity)
+
+        setSpecialIntstruction(item.specialInstructions)
 
     }
     console.log(selectedItem)
@@ -503,7 +507,7 @@ const Header = props => {
                                                                                     ${item.totalPrice}
                                                                                 </div>
                                                                                 <div className="editDelete">
-                                                                                    <svg onClick={() => { editItem(item.id, item.restaurantId, item.addOnObj, '', item.quantity) }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil edit" viewBox="0 0 16 16">
+                                                                                    <svg onClick={() => { editItem(item.id, item.restaurantId, item.addOnObj, '', item.quantity, item) }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil edit" viewBox="0 0 16 16">
                                                                                         <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                                                                     </svg>
                                                                                     <svg onClick={() => deleteItem(item.id, item.restaurantId, i)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill delete" viewBox="0 0 16 16">
@@ -609,6 +613,42 @@ const Header = props => {
                                                                     })
                                                                 : null
                                                             }
+                                                            <p>New Items</p>
+                                                            {!submitted && items.length ? items.filter((a, i) => a.status).map((item, i) => {
+                                                                return (
+                                                                    <>
+                                                                        <div className="details" key={i} onClick={() => console.log(item)}>
+                                                                            <div>
+                                                                                <div className="selected-quantity">
+                                                                                    {item.quantity}
+                                                                                </div>
+                                                                            </div>
+
+                                                                            <div className="item-description">
+                                                                                <div className="item-title">
+                                                                                    {item.name}
+                                                                                </div>
+
+                                                                                <div className="size-title">
+                                                                                    {item.shortDescription}
+                                                                                </div>
+                                                                            </div>
+
+                                                                            <div className="amount">
+                                                                                ${item.totalPrice}
+                                                                            </div>
+                                                                            <div className="editDelete">
+                                                                                <svg onClick={() => { editItem(item.id, item.restaurantId, item.addOnObj, i, item.quantity, item) }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil edit" viewBox="0 0 16 16">
+                                                                                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+                                                                                </svg>
+                                                                                <svg onClick={() => deleteItem(item.id, item.restaurantId, i)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill delete" viewBox="0 0 16 16">
+                                                                                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                                                                                </svg>
+                                                                            </div>
+                                                                        </div>
+                                                                    </>
+                                                                )
+                                                            }) : null}
                                                         </div>
                                                         <div className="lockItem">
                                                             <p>Submitted Item</p>
@@ -710,7 +750,7 @@ const Header = props => {
                         addedAddons={addedAddons}
                         editInded={editInded}
                         editedQuantity={editedQuantity}
-
+                        specialInstructions={specialInstructions}
                     />
                     :
                     null
